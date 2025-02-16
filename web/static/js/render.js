d2540c0ebd6899f9b8598cd857e710e38b40d5b8
loadept <falcorgd@gmail.com>
2025-02-15 22:12:23 -0500

    feat: change markdown paser

--- web/static/js/render.js
+++ web/static/js/render.js
@@ -1,15 +1,8 @@
-const converter = new showdown.Converter({
-  tables: true,
-  simplifiedAutoLink: true,
-  strikethrough: true,
-  tasklists: true,
-})
-converter.setFlavor('github')
-
 const content = document.getElementById('markdown')
 
 const text = content.textContent
-const html = converter.makeHtml(text)
+
+const parsed = marked.parse(text)
 
 content.remove()
-document.getElementById('output').innerHTML = html
+document.getElementById('output').innerHTML = parsed 
