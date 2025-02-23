ab7a219d32ac9e1ca52546a94cb763ae5ab4d208
loadept <falcorgd@gmail.com>
2025-02-22 23:50:01 -0500

    test: add logger middleware unit test

---
+++ api/middleware/logger_test.go
@@ -0,0 +1,32 @@
+package middleware_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/loadept/loadept.com/api/middleware"
+)
+
+func TestLoggerMiddleware(t *testing.T) {
+	testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	handler := middleware.LoggerMiddleware(testHandler)
+
+	req := httptest.NewRequest("GET", "/", nil)
+	rr := httptest.NewRecorder()
+
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Expected status 200, got %d", rr.Code)
+	}
+
+	expectedBody := "OK"
+	if rr.Body.String() != expectedBody {
+		t.Errorf("Expected body %s, got %q", expectedBody, rr.Body.String())
+	}
+}
