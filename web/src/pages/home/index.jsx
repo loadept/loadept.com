4afae64c078f5be332385990b5c05e538dd5fda4
loadept <falcorgd@gmail.com>
2025-03-03 09:07:04 -0500

    fix: spacing

--- web/src/pages/home/index.jsx
+++ web/src/pages/home/index.jsx
@@ -12,11 +12,11 @@ export const Index = () => {
           className="self-center md:justify-self-start justify-self-center text-sm md:text-base md:text-left text-center">
           <p>
             Aquí comparto lo que aprendo sobre <strong className="text-[#c678dd]">Linux</strong>,
-            <strong className="text-[#e06c75]">programación</strong>,
+            <strong className="text-[#e06c75]"> programación</strong>,
           </p>
           <p>
-            <strong className="text-[#98c379]">tecnología</strong>,
-            <strong className="text-[#61afef]">Go</strong> y más,
+            <strong className="text-[#98c379]"> tecnología</strong>,
+            <strong className="text-[#61afef]"> Go</strong> y más,
             con proyectos y notas en Markdown.
           </p>
           <p>
