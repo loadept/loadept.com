ebac13e92baecffc9e2887d9ef7553e4c63f1c54
loadept <falcorgd@gmail.com>
2025-03-06 17:23:15 -0500

    feat: add article model

---
+++ internal/model/article.go
@@ -0,0 +1,21 @@
+package model
+
+import "time"
+
+type ArticleModel struct {
+	UserModel
+	UserID      uint      `json:"user_id,omitempty"`
+	Title       string    `json:"title,omitempty"`
+	Description string    `json:"description,omitempty"`
+	Content     string    `json:"content,omitempty"`
+	CategoryID  uint      `json:"category_id,omitempty"`
+	Published   bool      `json:"published,omitempty"`
+	CreatedAt   *time.Time `json:"created_at,omitempty"`
+	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
+}
+
+type ArticleResponse struct {
+	Page string `json:"page,omitempty"`
+	Count string `json:"count"`
+	Data []*ArticleModel `json:"data"`
+}
