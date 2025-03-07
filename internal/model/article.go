a5b9f4c50aefe31c52ba7ef8e9fda2e8440225f6
loadept <falcorgd@gmail.com>
2025-03-07 17:38:27 -0500

    feat: add validation tags in model

--- internal/model/article.go
+++ internal/model/article.go
@@ -3,13 +3,13 @@ package model
 import "time"
 
 type ArticleModel struct {
-	UserModel
+	UserModel   `validate:"-"`
 	UserID      string     `json:"user_id,omitempty"`
-	Title       string     `json:"title,omitempty"`
-	Description string     `json:"description,omitempty"`
-	Content     string     `json:"content,omitempty"`
-	CategoryID  string     `json:"category_id,omitempty"`
-	Published   bool       `json:"published,omitempty"`
+	Title       string     `json:"title,omitempty" validate:"required,max=36,min=1"`
+	Description string     `json:"description,omitempty" validate:"required,max=255,min=1"`
+	Content     string     `json:"content,omitempty" validate:"required,min=1"`
+	CategoryID  string     `json:"category_id,omitempty" validate:"uuid"`
+	Published   bool       `json:"published,omitempty" validate:"boolean"`
 	CreatedAt   *time.Time `json:"created_at,omitempty"`
 	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
 }
--- internal/model/category.go
+++ internal/model/category.go
@@ -4,7 +4,7 @@ type CategoryModel struct {
 	Model
 	Name        string `json:"name,omitempty" validate:"required,max=50,min=1"`
 	Description string `json:"description,omitempty" validate:"required,max=255,min=6"`
-	HexColor    string `json:"hex_color,omitempty" validate:"required,max=7,min=7"`
+	HexColor    string `json:"hex_color,omitempty" validate:"required,hexcolor"`
 	NerdIcon    string `json:"nerd_icon,omitempty" validate:"required,max=10,min=1"`
 }
 
