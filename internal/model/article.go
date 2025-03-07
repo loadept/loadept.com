fbced3dc2acbd9228ead3ce91b1c5a1285586725
loadept <falcorgd@gmail.com>
2025-03-06 23:33:17 -0500

    fix: use correct type in field

--- internal/model/article.go
+++ internal/model/article.go
@@ -4,11 +4,11 @@ import "time"
 
 type ArticleModel struct {
 	UserModel
-	UserID      uint       `json:"user_id,omitempty"`
+	UserID      string     `json:"user_id,omitempty"`
 	Title       string     `json:"title,omitempty"`
 	Description string     `json:"description,omitempty"`
 	Content     string     `json:"content,omitempty"`
-	CategoryID  uint       `json:"category_id,omitempty"`
+	CategoryID  string     `json:"category_id,omitempty"`
 	Published   bool       `json:"published,omitempty"`
 	CreatedAt   *time.Time `json:"created_at,omitempty"`
 	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
