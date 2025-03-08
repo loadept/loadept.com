3a7354ab04290f0558446d6f300f3f3490144393
loadept <falcorgd@gmail.com>
2025-03-08 08:55:56 -0500

    feat: improve user model

--- internal/model/user.go
+++ internal/model/user.go
@@ -4,10 +4,14 @@ import "time"
 
 type UserModel struct {
 	Model
-	FullName  string     `json:"full_name,omitempty"`
-	Username  string     `json:"username,omitempty"`
-	Email     string     `json:"email,omitempty"`
-	Password  string     `json:"password,omitempty"`
+	Username  string     `json:"username,omitempty" validate:"required,max=50,min=1"`
+	Password  string     `json:"password,omitempty" validate:"required,max=255,min=6"`
 	CreatedAt *time.Time `json:"created_at,omitempty"`
 	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+}
+
+type RegisterUserModel struct {
+	UserModel
+	FullName  string     `json:"full_name,omitempty" validate:"required,max=100,min=1"`
+	Email     string     `json:"email,omitempty" validate:"required,email"`
 }
