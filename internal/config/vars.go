5f4385d3794d8bb0837b370d03b78069c9d0601c
loadept <falcorgd@gmail.com>
2025-03-09 13:13:18 -0500

    feat: add env variable

--- internal/config/vars.go
+++ internal/config/vars.go
@@ -21,4 +21,5 @@ type config struct {
 	PORT      string `env:"PORT" default:"8080"`
 	DB_NAME   string `env:"DB_NAME" default:"db.sqlite"`
 	REDIS_URI string `env:"REDIS_URI" default:""`
+	SECRET_KEY string `env:"SECRET_KEY" default:""`
 }
