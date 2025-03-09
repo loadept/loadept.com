35617c0822d629537d4783216ac26592d46ad2cf
loadept <falcorgd@gmail.com>
2025-03-09 14:50:14 -0500

    feat: add token service for dependency injection in middleware

--- api/middleware/auth.go
+++ api/middleware/auth.go
@@ -5,44 +5,46 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/loadept/loadept.com/internal/auth/jwt"
+	"github.com/loadept/loadept.com/internal/auth"
 	"github.com/loadept/loadept.com/pkg/respond"
 )
 
 const AuthContextKey = "AuthContext"
 
-func AuthMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		headerToken := r.Header.Get("Authorization")
-		if !strings.HasPrefix(headerToken, "Bearer ") {
-			respond.JSON(w, respond.Map{
-				"detail": "Resource not found",
-			}, http.StatusNotFound)
-			return
-		}
+func NewAuthMiddleware(authService auth.TokenService) Middleare {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			headerToken := r.Header.Get("Authorization")
+			if !strings.HasPrefix(headerToken, "Bearer ") {
+				respond.JSON(w, respond.Map{
+					"detail": "Resource not found",
+				}, http.StatusNotFound)
+				return
+			}
 
-		token := strings.TrimPrefix(headerToken, "Bearer ")
+			token := strings.TrimPrefix(headerToken, "Bearer ")
 
-		claims, err := jwt.JWTAuth().ValidateToken(token)
-		if err != nil {
-			respond.JSON(w, respond.Map{
-				"detail": "The token is not valid",
-			}, http.StatusUnauthorized)
-			return
-		}
+			claims, err := authService.ValidateToken(token)
+			if err != nil {
+				respond.JSON(w, respond.Map{
+					"detail": "The token is not valid",
+				}, http.StatusUnauthorized)
+				return
+			}
 
-		if !claims.IsAdmin {
-			respond.JSON(w, respond.Map{
-				"detail": "The token is not valid",
-			}, http.StatusUnauthorized)
-			return
-		}
+			if !claims.IsAdmin {
+				respond.JSON(w, respond.Map{
+					"detail": "The token is not valid",
+				}, http.StatusUnauthorized)
+				return
+			}
 
-		ctx := context.WithValue(r.Context(), AuthContextKey, respond.Map{
-			"user_id":  claims.UserID,
-			"is_admin": claims.IsAdmin,
+			ctx := context.WithValue(r.Context(), AuthContextKey, respond.Map{
+				"user_id":  claims.UserID,
+				"is_admin": claims.IsAdmin,
+			})
+
+			next.ServeHTTP(w, r.WithContext(ctx))
 		})
-
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
+	}
 }
