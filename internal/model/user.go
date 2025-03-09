d1cc51ab044828182f01e4ce30069d787eba066d
loadept <falcorgd@gmail.com>
2025-03-09 10:02:23 -0500

    update user model

--- internal/model/user.go
+++ internal/model/user.go
@@ -4,14 +4,16 @@ import "time"
 
 type UserModel struct {
 	Model
-	Username  string     `json:"username,omitempty" validate:"required,max=50,min=1"`
-	Password  string     `json:"password,omitempty" validate:"required,max=255,min=6"`
+	Username  string `json:"username,omitempty" validate:"required,max=50,min=1"`
+	Password  string `json:"password,omitempty" validate:"required,max=255,min=6"`
+	IsAdmin   bool
+	IsActive  bool
 	CreatedAt *time.Time `json:"created_at,omitempty"`
 	UpdatedAt *time.Time `json:"updated_at,omitempty"`
 }
 
 type RegisterUserModel struct {
 	UserModel
-	FullName  string     `json:"full_name,omitempty" validate:"required,max=100,min=1"`
-	Email     string     `json:"email,omitempty" validate:"required,email"`
+	FullName string `json:"full_name,omitempty" validate:"required,max=100,min=1"`
+	Email    string `json:"email,omitempty" validate:"required,email"`
 }
