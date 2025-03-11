825d68194aa0e67e3dab2bd8ae709a2b09881ea1
loadept <falcorgd@gmail.com>
2025-03-11 18:57:52 -0500

    feat: register routes / chore

--- cmd/loadept/main.go
+++ cmd/loadept/main.go
@@ -1,100 +1,107 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/go-playground/validator/v10"
 	"github.com/loadept/loadept.com/api"
 	"github.com/loadept/loadept.com/api/handler"
 	"github.com/loadept/loadept.com/api/middleware"
-	"github.com/loadept/loadept.com/internal/auth"
 	"github.com/loadept/loadept.com/internal/config"
-	"github.com/loadept/loadept.com/internal/database"
-	"github.com/loadept/loadept.com/internal/repository"
+	"github.com/loadept/loadept.com/internal/infraestucture/cache"
+	"github.com/loadept/loadept.com/internal/infraestucture/database"
 	"github.com/loadept/loadept.com/internal/service"
+	"github.com/loadept/loadept.com/pkg/util"
+	"github.com/redis/go-redis/v9"
 )
 
-var conn *sql.DB
+var (
+	conn *sql.DB
+	rdb  *redis.Client
+	ctx  = context.Background()
+)
 
 func init() {
 	config.LoadConfig()
 
-	db, err := database.NewConnection()
-	if err != nil {
-		log.Println("Error to connect with database")
-		os.Exit(1)
+	{
+		db, err := database.NewConnection()
+		if err != nil {
+			log.Println("Error to connect with database")
+			os.Exit(1)
+		}
+
+		now, err := db.GetNow()
+		if err != nil {
+			log.Println("Error to optain current date")
+			os.Exit(1)
+		}
+
+		formatDate := now.Format("2006-01-02")
+		log.Printf("Connection with database success, current date %s\n", formatDate)
+
+		conn = db.GetDB()
 	}
+	{
+		redisClient, err := cache.NewRedisConnection(ctx)
+		if err != nil {
+			log.Println("Error to connect with redis:", err)
+			os.Exit(1)
+		}
 
-	now, err := db.GetNow()
-	if err != nil {
-		log.Println("Error to optain current date")
-		os.Exit(1)
+		redisDate, err := redisClient.GetNow()
+		if err != nil {
+			log.Println("Error to optain current date from redis:", err)
+			os.Exit(1)
+		}
+
+		formatRedisDate := redisDate.Format("2006-01-02")
+		log.Printf("Connection with redis success, current date %s\n", formatRedisDate)
+
+		rdb = redisClient.GetClient()
 	}
-
-	formatDate := now.Format("2006-01-02")
-	log.Printf("Connection with database success, current date %s\n", formatDate)
-
-	conn = db.GetDB()
 }
 
 func main() {
 	mux := http.NewServeMux()
+	httpClient := &http.Client{}
 
-	validate := validator.New()
-
-	authService := auth.NewAuthService("JWT")
-	authMiddleware := middleware.NewAuthMiddleware(authService)
-
-	userRepo := repository.NewUserRepository(conn)
-	userService := service.NewUserService(userRepo, validate)
-	userHandler := handler.NewApiUserHandler(userService, authService)
-
-	articleRepo := repository.NewArticleRepository(conn)
-	articleService := service.NewArticleService(articleRepo, validate)
+	articleService := service.NewArticleService(httpClient, rdb, ctx)
 	handlerArticles := handler.NewArticlesHandler(articleService)
 
-	categoryRepo := repository.NewCetogoryRepository(conn)
-	categoryService := service.NewCategoryService(categoryRepo, validate)
+	categoryService := service.NewCategoryService(httpClient, rdb, ctx)
 	categoryHandler := handler.NewApiCategoryHandler(categoryService)
 
-	// File Server
-	mux.Handle("/", middleware.GzipEncoding(api.ServeSPA("web/dist", "index.html")))
-
-	// Accessible routes
 	{
-		mux.HandleFunc("/api/category/{id}", categoryHandler.GetCategoryByID)
+		mux.Handle("/", middleware.GzipEncoding(api.ServeSPA("web/dist", "index.html")))
+
 		mux.HandleFunc("/api/category", categoryHandler.GetCategories)
-
-		mux.HandleFunc("/api/article/recent", handlerArticles.GetRecentArticles)
-		mux.HandleFunc("/api/article/{id}", handlerArticles.GetArticleByID)
-		mux.HandleFunc("/api/article", handlerArticles.GetArticles)
-
-		mux.HandleFunc("/api/auth/register", userHandler.RegisterUser)
-		mux.HandleFunc("/api/auth/login", userHandler.LoginUser)
+		mux.HandleFunc("/api/article/{category}", handlerArticles.GetArticles)
+		mux.HandleFunc("/api/article/{category}/{name}", handlerArticles.GetArticleBySha)
+		mux.HandleFunc("/api/article/edit/{category}/{name}", handlerArticles.EditArticle)
 	}
 
-	// Protected router for admin
-	{
-		mux.Handle("/api/category/register", authMiddleware(
-			http.HandlerFunc(categoryHandler.RegisterCategory),
-		))
-		mux.Handle("/api/article/register", authMiddleware(
-			http.HandlerFunc(handlerArticles.RegisterArticle),
-		))
+	var muxWrapped http.Handler
+	if util.ParseBool(config.Env.DEBUG) {
+		log.Println("You are in \033[33mdebug\033[0m mode, cors middleware will be used")
+		muxWrapped = middleware.CorsMiddleware(
+			middleware.LoggerMiddleware(mux),
+		)
+	} else {
+		muxWrapped = middleware.LoggerMiddleware(mux)
 	}
 
 	server := http.Server{
 		Addr:    fmt.Sprintf(":%s", config.Env.PORT),
-		Handler: middleware.LoggerMiddleware(mux),
+		Handler: muxWrapped,
 	}
 
-	log.Printf("\033[32mServer ready to listen on addr %s\033[0m\n", config.Env.PORT)
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Printf("\033[31mError to listen serve\033[0m: %v\n", err)
+	log.Printf("\033[32mServer ready to listen on addr %s\033[0m\n", server.Addr)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("\033[31mError to listen serve\033[0m: %v\n", err)
 	}
 }
