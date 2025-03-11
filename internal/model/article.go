15b73817760573b2a63d229ca887e0c969684a86
loadept <falcorgd@gmail.com>
2025-03-11 09:21:27 -0500

    feat: update article model

--- internal/model/article.go
+++ internal/model/article.go
@@ -3,19 +3,12 @@ package model
 import "time"
 
 type ArticleModel struct {
-	UserModel   `validate:"-"`
-	UserID      string     `json:"user_id,omitempty"`
-	Title       string     `json:"title,omitempty" validate:"required,max=36,min=1"`
-	Description string     `json:"description,omitempty" validate:"required,max=255,min=1"`
-	Content     string     `json:"content,omitempty" validate:"required,min=1"`
-	CategoryID  string     `json:"category_id,omitempty" validate:"required,uuid"`
-	Published   bool       `json:"published,omitempty" validate:"boolean"`
-	CreatedAt   *time.Time `json:"created_at,omitempty"`
-	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
+	Name      string    `json:"name,omitempty" validate:"required,max=36,min=1"`
+	Path      string    `json:"path,omitempty"`
+	Sha       string    `json:"sha,omitempty"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type ArticleResponse struct {
-	Page  string          `json:"page,omitempty"`
-	Count string          `json:"count"`
-	Data  []*ArticleModel `json:"data"`
+	Articles []ArticleModel `json:"articles"`
 }
