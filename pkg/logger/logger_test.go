3c6fa1311f2edef9678754d3023f2dcf16e85b30
loadept <falcorgd@gmail.com>
2025-06-15 16:25:07 -0500

    test: add test for logger

---
+++ pkg/logger/logger_test.go
@@ -0,0 +1,41 @@
+package logger
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewLogger(t *testing.T) {
+	err := os.MkdirAll("logs", os.ModePerm)
+	assert.NoError(t, err, "Should be able to create the directory records")
+
+	defer os.RemoveAll("logs")
+
+	NewLogger()
+	INFO.Println("Test message")
+	ERROR.Println("Test error message")
+
+	CloseLogger()
+
+	currentDate := time.Now().Format("2006-01-02")
+	infoPath := filepath.Join("logs", "access-"+currentDate+".log")
+	errorPath := filepath.Join("logs", "error-"+currentDate+".log")
+
+	_, err = os.Stat(infoPath)
+	assert.NoError(t, err, "Info file should exist")
+
+	_, err = os.Stat(errorPath)
+	assert.NoError(t, err, "Error file should exist")
+
+	infoContent, err := os.ReadFile(infoPath)
+	assert.NoError(t, err)
+	assert.Contains(t, string(infoContent), "Test message")
+
+	errorContent, err := os.ReadFile(errorPath)
+	assert.NoError(t, err)
+	assert.Contains(t, string(errorContent), "Test error message")
+}
