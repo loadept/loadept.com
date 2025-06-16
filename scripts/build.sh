7ee53679f91c17417654f1046278dec54c7236d0
loadept <falcorgd@gmail.com>
2025-06-15 21:46:56 -0500

    ci: add production workflow

---
+++ scripts/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+export CGO_ENABLED=1
+
+APP_NAME=loadept-$(date +"%d-%m-%y").app
+DIR_NAME=loadept-$(date +"%d-%m-%y")
+
+pushd ./web > /dev/null
+if ! pnpm run build; then
+    echo "Build static failed"
+    exit 1
+fi
+popd > /dev/null
+
+if ! go build -o $APP_NAME cmd/loadept/main.go; then
+    echo "Build binary failed"
+    exit 1
+fi
+
+mkdir -p $DIR_NAME/logs
+mkdir $DIR_NAME/web
+
+mv $APP_NAME $DIR_NAME
+mv web/dist $DIR_NAME/web
+
+tar -czvf $DIR_NAME.tar.gz $DIR_NAME
+
+rm -rf $DIR_NAME
