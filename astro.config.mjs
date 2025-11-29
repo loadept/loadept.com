e83caedbf28b8c515365afd75d1887e960261fbc
loadept <loadept3@gmail.com>
2025-11-28 22:21:14 -0500

    feat: remove unused assets and components, add new font and styles
    - Deleted `astro.svg` and `background.svg` assets.
    - Added new font file `firacode.woff2`.
    - Introduced multiple glitch images for background effects.
    - Removed `Welcome.astro` component and its associated styles.
    - Created `global.css` for global styles and typography settings.
    - Updated `tsconfig.json` to enhance TypeScript strictness and JSX support.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import preact from '@astrojs/preact';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://loadept.com',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [preact({ compat: true }), sitemap()],
+});
